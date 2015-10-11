2731ab9d2fdd27e8b8d322e10bbad16cd9b38025
Manish <manishsingh10895@gmail.com>
2015-10-11 16:37:30 +0530

    Class Diagram Added

---
+++ ExternalResources/Apparatuses/ContainerApparatus.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ExternalResources.Apparatuses
+{
+    public class ContainerApparatus : UserControl
+    {
+        public ContainerApparatus()
+        {
+            Volume = 0;
+            ChemicalNames = new ObservableCollection<string>();
+            ChemicalName = "";
+        }
+        public static readonly DependencyProperty VolumeProperty = DependencyProperty.Register("Volume", typeof(double), typeof(ContainerApparatus), new PropertyMetadata(0.0));
+        public double Volume
+        {
+            get { return (double)GetValue(VolumeProperty); }
+            set { SetValue(VolumeProperty, value); }
+        }
+
+        public static readonly DependencyProperty ChemicalNameProperty = DependencyProperty.Register("ChemicalName", typeof(string), typeof(ContainerApparatus));
+        public string ChemicalName
+        {
+            get { return (string)GetValue(ChemicalNameProperty); }
+            set { SetValue(ChemicalNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty ChemicalNamesProperty = DependencyProperty.Register("ChemicalNames", typeof(ObservableCollection<string>), typeof(ContainerApparatus));
+        public ObservableCollection<string> ChemicalNames
+        {
+            get { return (ObservableCollection<string>)GetValue(ChemicalNamesProperty); }
+            set
+            {
+                SetValue(ChemicalNamesProperty, value);
+            }
+        }
+            
+    }
+}
